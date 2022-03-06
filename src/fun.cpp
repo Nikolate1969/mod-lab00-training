58c2abca6976d111e09e014a13eb3af0c7a70d90
Nikolate1969 <100879984+Nikolate1969@users.noreply.github.com>
2022-03-06 22:26:01 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) {
-    return 0;
+int64_t power(int64_t x, uint16_t n)
+{
+  int64_t k = x;
+  for (int i = 0; i < n - 1; i++) 
+  {
+      x *= k;
+  }
+  return x;  
 }
